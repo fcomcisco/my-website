88d4704c8418a56b32cba045c39bc1097d458726
fcomcisco <francisco.montecinos1999@gmail.com>
2025-03-11 16:26:01 -0300

    gh update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/My-website",     
+  assetPrefix: "/My-website",  
   images: {
-    unoptimized: true,
+    unoptimized: true,         
   },
 };
 
