702e8ca0f15d59fcc7b13e77438502dd93c090e3
fcomcisco <francisco.montecinos1999@gmail.com>
2025-03-14 11:15:25 -0300

    try2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/my-website",
+  assetPrefix: "/my-website",
   trailingSlash: true,
   images: {
     unoptimized: true,
