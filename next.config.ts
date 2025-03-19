dce183c35138a3ccde6a4675d622927e457f29d4
fcomcisco <francisco.montecinos1999@gmail.com>
2025-03-19 18:46:45 -0300

    try2

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/my-website" : "",
+  assetPrefix: isProd ? "/my-website" : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
