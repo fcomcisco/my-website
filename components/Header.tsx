71320fde91dc07ff159704db9620546cdd79cc36
fcomcisco <francisco.montecinos1999@gmail.com>
2025-04-15 18:29:11 -0400

    projects pt 1 images fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Link from "next/link";
 
 export default function Header() {
   const [activeSection, setActiveSection] = useState("profile");
